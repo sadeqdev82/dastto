7dc207ef748dd080a2885b31ffd48c596e03a54e
mohamadreza <mohamadreza1388.org@gmail.com>
2024-11-29 02:55:32 +0330

    feat(progress): add progress bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import "nprogress/nprogress.js"
-import "nprogress/nprogress.css"
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import "./styles/index.css"
 import HomeLayouts from "./layoutes/pages/HomeLayouts.jsx";
+import RouteChangeHandler from "./components/shared/RouteChangeHandler.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(<StrictMode>
     <Router>
-      <Routes>
-        <Route path="/" element={<HomeLayouts />} />
-      </Routes>
+        <RouteChangeHandler />
+        <Routes>
+            <Route path="/" element={<HomeLayouts/>}/>
+        </Routes>
     </Router>
-  </StrictMode>,
-)
+</StrictMode>,)
