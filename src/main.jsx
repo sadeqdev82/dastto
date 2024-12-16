6633b9d31fcc6060804b1b2c09139f157f6d4091
mohamadreza <mohamadreza1388.org@gmail.com>
2024-12-16 18:31:13 +0330

    feat(style)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import "./styles/index.css"
 import HomeLayouts from "./layoutes/pages/HomeLayouts.jsx";
 import RouteChangeHandler from "./components/shared/RouteChangeHandler.jsx";
 import NotFound from "./components/errors/NotFound.jsx";
